526835b946c1d0c357236a89b3a6ed1944f4da17
Logan Lessen <loganlessen@gmail.com>
2024-10-09 12:53:23 -0600

    First attempt

--- generate_password.js
+++ generate_password.js
@@ -1,5 +1,10 @@
 function generatePassword(length = 12) {
-    // complete me
+    const validChars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890_";
+    let password = "";
+    for(let i = 0; i < length; i++){
+        password += validChars[Math.floor(Math.random() * validChars.length)]
+    }
+    return password
 }
 
 module.exports = generatePassword;
